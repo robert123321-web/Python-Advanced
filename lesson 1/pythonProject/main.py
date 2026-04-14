0160f8a3b43f00e243bbb4032f1473aedad23f38
robert123321-web <roberthaxhiu2012@gmail.com>
2026-04-14 19:57:00 +0200

    lesson 2

---
+++ lesson 1/pythonProject/main.py
@@ -0,0 +1,21 @@
+name = "Robert"
+surname = "Haxhiu"
+age = 13
+temperature
+
+print (users)
+
+#print (users[0])
+ = 37.5
+
+full_name = name + " " + surname
+users = ["Robert","Viki","Patrik"]
+#users.append = ("Roberti")
+# users.reverse ()
+#users.insert (_index)
+ # print (full_name)
+# print(name,age,temperature)
+
+# print (type(name))
+# print (type(age))
+
