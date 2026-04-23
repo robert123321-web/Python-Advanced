6af7f6fe8677bfea01ba9efc31def646111966f0
robert123321-web <roberthaxhiu2012@gmail.com>
2026-04-23 20:38:29 +0200

    lesson 5

---
+++ lesson 5/lesson 5/defaulArgs.py
@@ -0,0 +1,10 @@
+
+
+def sayHello (name,greeting = "hello"):
+    print(name,greeting)
+
+
+
+sayHello("roberti")
+
+sayHello("donjeta","hello helllo cka po bon")
---
+++ lesson 5/lesson 5/globalVar.py
@@ -0,0 +1,17 @@
+
+
+def test():
+    name = "Lisa"
+    print(name)
+
+
+test()
+
+shlolla = "DigitalSchool"
+
+def test2():
+    global shkolla
+    shkolla = "Harvard"
+
+test2()
+print(shkolla)
