7fb5082ccaf546080fd9c908f70371581a61bc12
robert123321-web <roberthaxhiu2012@gmail.com>
2026-05-21 20:46:12 +0200

    lesson 13

---
+++ lesson 13/lesson 13/test.py
@@ -0,0 +1,13 @@
+import test as pd
+
+product = ["Apples","Bananas","Oranges","Grapes","Pineapples"]
+
+sales = [150,200,180,90,60]
+
+sales_series = pd.Series(sales,index=product)
+
+
+print(sales_series)
+
+print(sales_series["Grapes"])
+
